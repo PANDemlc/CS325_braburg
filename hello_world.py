8a063c58004311917ca7c8e0fa0e6a01ce240638
PANDemlc <braden.burgener@gmail.com>
2024-01-26 13:43:01 -0600

    Changed num generator to only choose even numbers

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
 import random
 
-# Random numer generator from 1-10
-num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Even random number generator from 1-10
+num_list = [2, 4, 6, 8, 10]
 random_num = random.choice(num_list)
+
+print(f"Random Even Number: {random_num}")
+
+random_num = random_num / 2
+
+print(f"Random Even Number Divided by 2: {random_num}")
